67d571588c8d525324f42cbab658227343b497f8
guilhermeurnau <gaudstoo@hotmail.com>
2017-10-31 19:41:53 -0200

    Arquivos GitHub

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { Stundent } from './student/student.model'
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'jad';
+
+  students : Stundent[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant' },
+    {name: 'Han Solo', isJedi: false },
+    {name: 'Leia', isJedi: false }
+  ]
 }
---
+++ src/app/student/student.model.ts
@@ -0,0 +1,5 @@
+export interface Stundent {
+    name: string
+    isJedi: boolean
+    temple?: string  
+}
